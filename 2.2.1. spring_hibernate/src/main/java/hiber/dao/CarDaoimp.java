c867720b2717c78132bc1e7374372a499f1d4cf4
r4181126-web <r4181126@gmail.com>
2026-02-22 19:56:45 +0500

    add CarDAO

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoimp.java
@@ -0,0 +1,33 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class CarDaoimp implements CarDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public CarDaoimp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(Car car) {
+        sessionFactory.getCurrentSession().save(car);
+
+    }
+
+    @Override
+    public List<Car> listCars() {
+        TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car", Car.class);
+        return query.getResultList();
+    }
+
+}
